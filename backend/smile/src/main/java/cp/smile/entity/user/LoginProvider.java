2b8e4f63a0e2c21418e8341c4104dcb1850f11a7
lsh80165 <lsh80165@gmail.com>
2023-01-31 17:08:08 +0900

    Fix : Login Provider 엔티티에서 Provider를 enum으로 매핑해서 로그인 에러나는 문제 해결

--- backend/smile/src/main/java/cp/smile/entity/user/LoginProvider.java
+++ backend/smile/src/main/java/cp/smile/entity/user/LoginProvider.java
@@ -1,5 +1,6 @@
 package cp.smile.entity.user;
 
+import cp.smile.auth.oauth2.provider.OAuth2Provider;
 import cp.smile.config.BaseEntity;
 import lombok.Builder;
 import lombok.Getter;
@@ -19,12 +20,13 @@ public class LoginProvider extends BaseEntity {
     @Column(name = "lp_id")
     private int id;
     @Column(name = "lp_provider")
-    private String provider;
+    @Enumerated(EnumType.STRING)
+    private OAuth2Provider provider;
 
     /*인증 제공자 쪽에서 유저 정보를 조회할 일은 없기 때문에 연관관계를 맺지 않음.*/
 
     @Builder
-    public LoginProvider(int id, String provider) {
+    public LoginProvider(int id, OAuth2Provider provider) {
         this.id = id;
         this.provider = provider;
     }
