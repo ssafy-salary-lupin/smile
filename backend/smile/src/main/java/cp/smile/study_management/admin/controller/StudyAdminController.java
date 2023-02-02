99193a93991dffe24d6438faee73cb3f7cac511a
yyh0126 <yyh0126@naver.com>
2023-02-03 01:05:41 +0900

    Fix : 스터디에 속한 회원조회

---
+++ backend/smile/src/main/java/cp/smile/study_management/admin/controller/StudyAdminController.java
@@ -0,0 +1,45 @@
+package cp.smile.study_management.admin.controller;
+
+import cp.smile.config.response.DataResponse;
+import cp.smile.config.response.ResponseService;
+import cp.smile.entity.user.User;
+import cp.smile.study_management.admin.dto.response.FindStudyJoinedUserDTO;
+import cp.smile.study_management.admin.service.StudyAdminService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+public class StudyAdminController {
+
+    private final StudyAdminService studyAdminService;
+    private final ResponseService responseService;
+
+    @GetMapping("/studies/{studyId}/users")
+    public DataResponse<List<FindStudyJoinedUserDTO>> findStudyJoinedUser(@PathVariable int studyId) {
+
+        List<User> users = studyAdminService.findUserByStudy(studyId);
+
+        List<FindStudyJoinedUserDTO> FindStudyJoinedUserDTOS = new ArrayList<>();
+
+        for (User user : users) {
+
+
+
+            FindStudyJoinedUserDTO.builder()
+                    .id(user.getId())
+                    .email(user.getEmail())
+                    .nickname(user.getNickname())
+                    .isLeader();
+        }
+
+        return responseService.getDataResponse(FindStudyJoinedUserDTOS);
+    }
+}
--- backend/smile/src/main/java/cp/smile/user/repository/UserRepository.java
+++ backend/smile/src/main/java/cp/smile/user/repository/UserRepository.java
@@ -2,7 +2,9 @@ package cp.smile.user.repository;
 
 import cp.smile.entity.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 
@@ -12,5 +14,8 @@ public interface UserRepository extends JpaRepository<User, Integer> {
 
     Optional<User> findById(int id);
 
-
+    @Query(value = "select u from User u " +
+            "join fetch UserJoinStudy ujs " +
+            "where ujs.studyInformation.id = :studyId")
+    List<User> findUserByStudy(int studyId);
 }
