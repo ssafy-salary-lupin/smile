41b02ecaddef6c42ea945ec4bfabed448a307247
lsh80165 <lsh80165@gmail.com>
2023-02-03 17:35:04 +0900

    Fix : COR 해결 - 추후에 제거할 예정

---
+++ backend/smile/src/main/java/cp/smile/config/redis/EmbeddedRedisConfig.java
@@ -0,0 +1,32 @@
+package cp.smile.config.redis;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import redis.embedded.RedisServer;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+@Profile("local")
+@Configuration
+public class EmbeddedRedisConfig {
+
+    @Value("${spring.redis.port}")
+    private int redisPort;
+
+    private RedisServer redisServer;
+
+    @PostConstruct
+    public void redisServer(){
+        redisServer = new RedisServer(redisPort);
+        redisServer.start();
+    }
+
+    @PreDestroy
+    public void stopRedis(){
+        if(redisServer != null){
+            redisServer.stop();
+        }
+    }
+}
