b819497ad8045e37f7b3237964bbc8c01d89480b
lsh80165 <lsh80165@gmail.com>
2023-02-06 09:56:18 +0900

    Fix : 리소스 파일 수정

--- backend/smile/src/main/java/cp/smile/config/AWSConfig.java
+++ backend/smile/src/main/java/cp/smile/config/AWSConfig.java
@@ -10,7 +10,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-
+//aws 설정
 @Configuration
 public class AWSConfig {
     @Value("${cloud.aws.credentials.access-key}")
