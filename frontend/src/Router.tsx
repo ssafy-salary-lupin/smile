1b37b4b5381c08b50cc69c2691043f7cb21de01c
songkira <thdrlfk93@naver.com>
2023-02-06 17:25:40 +0900

    Feat : 화상회의 페이지 기본형 커스텀ing

--- frontend/src/Router.tsx
+++ frontend/src/Router.tsx
@@ -5,14 +5,11 @@ import StudyCreatePages from "./pages/StudyCreatePages";
 import TestPages from "./pages/TestPages";
 import VideoRoomComponent from "components/video-meeting/VideoRoomComponent";
 import WaitingPages from "./pages/WaitingPages";
-<<<<<<< HEAD
 // import Settings from "pages/Settings";
 // import Settings from "./pages/Settings";
-=======
 import CreateReadChat from "components/study-manage/CreateReadChat";
 // import CreateReadChat from "components/study-manage/CreateReadChat";
 // import StudyChat from "components/study-manage/CreateReadChat";
->>>>>>> 9e4732cec29f989c0c48743fac0ceee607551055
 function Router() {
   return (
     <BrowserRouter>
