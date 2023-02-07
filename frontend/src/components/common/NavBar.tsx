8aa179e046a7a15c3edd0af91e943266629031bf
정혜주 <fixup719@gmail.com>
2023-02-07 13:14:07 +0900

    Test : kakao test

--- frontend/src/components/common/NavBar.tsx
+++ frontend/src/components/common/NavBar.tsx
@@ -194,7 +194,11 @@ function NavBar(props: UrlProps) {
           {/* <Item>
             <NabBtn>로그아웃</NabBtn>
           </Item> */}
-          <NabBtn onClick={kakaoTest}>로그인</NabBtn>
+          <NabBtn>
+            <a href="https://i8b205.p.ssafy.io/be-api/oauth2/authorization/kakao">
+              로그인
+            </a>
+          </NabBtn>
         </Items>
       </LinksContainer>
     </Nav>
