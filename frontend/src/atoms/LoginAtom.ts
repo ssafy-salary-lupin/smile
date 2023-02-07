9e46e0b7b5a61b467de3246bc811508c27792f4c
정혜주 <fixup719@gmail.com>
2023-02-07 14:59:54 +0900

    Test : kakaoTest

--- frontend/src/atoms/LoginAtom.ts
+++ frontend/src/atoms/LoginAtom.ts
@@ -4,13 +4,3 @@ export const LoginToken = atom({
   key: "token",
   default: "",
 });
-
-// 값 잘 넘어오는 지 테스트용
-export const LoginSelector = selector({
-  key: "LoginSelectorTest",
-  get: ({ get }) => {
-    // options의 매개변수에 있는 {get} function 가져오기
-    const data = get(LoginToken);
-    return data;
-  },
-});
