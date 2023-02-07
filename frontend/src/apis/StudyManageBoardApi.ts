21ee077459405e97c1ff345b1c9e2ba1eec7ef56
정혜주 <fixup719@gmail.com>
2023-02-07 09:42:03 +0900

    Feat : 게시판 목록 api 수정

--- frontend/src/apis/StudyManageBoardApi.ts
+++ frontend/src/apis/StudyManageBoardApi.ts
@@ -5,13 +5,16 @@ const BASE_URL = `https://i8b205.p.ssafy.io/be-api/studies`;
 export async function boardListSelectAllApi(page: number, size: number) {
   try {
     console.log("Get 실행");
-    const response = await fetch(`${BASE_URL}/1/boards?page=1&size=10`, {
-      headers: {
-        Authorization:
-          "Bearer eyJhbGciOiJIUzUxMiJ9.eyJyb2xlIjoiUk9MRV9VU0VSIiwidXNlckVtYWlsIjoiZG9pdGZvcmp1bmdAa2FrYW8uY29tIiwidXNlcklkIjozLCJpc3MiOiJpc3N1ZXIiLCJpYXQiOjE2NzU3Mjg3MzIsImV4cCI6MTY3NTczMjMzMn0.VwUVfFJBvWXuXRYQx8_dOqJr_iaNcohu6EiiMiWK_yqyHBLUP2eQZgG1YWyvgvLqurAsP6iWf5nSzbPnUr75zg",
-        Accept: "application/json",
+    const response = await fetch(
+      `${BASE_URL}/1/boards?page=${page}&size=${size}`,
+      {
+        headers: {
+          Authorization:
+            "Bearer eyJhbGciOiJIUzUxMiJ9.eyJyb2xlIjoiUk9MRV9VU0VSIiwidXNlckVtYWlsIjoiZG9pdGZvcmp1bmdAa2FrYW8uY29tIiwidXNlcklkIjozLCJpc3MiOiJpc3N1ZXIiLCJpYXQiOjE2NzU3Mjg3MzIsImV4cCI6MTY3NTczMjMzMn0.VwUVfFJBvWXuXRYQx8_dOqJr_iaNcohu6EiiMiWK_yqyHBLUP2eQZgG1YWyvgvLqurAsP6iWf5nSzbPnUr75zg",
+          Accept: "application/json",
+        },
       },
-    });
+    );
 
     console.log("response : ", response);
     const data = await response.json();
