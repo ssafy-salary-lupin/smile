d8b0349448eb673a1519dc0a1fa9501b16c643e4
정혜주 <fixup719@gmail.com>
2023-02-07 09:29:25 +0900

    Fix : api url 수정

--- frontend/src/apis/StudyManageBoardApi.ts
+++ frontend/src/apis/StudyManageBoardApi.ts
@@ -1,11 +1,11 @@
 import axios from "axios";
 
 const BASE_URL = `https://i8b205.p.ssafy.io/be-api/studies`;
-
+//
 export async function boardListSelectAllApi(page: number, size: number) {
   try {
     console.log("Get 실행");
-    const response = await fetch(`${BASE_URL}/1/boards/page=1&size=10`, {
+    const response = await fetch(`${BASE_URL}/1/boards?page=1&size=10`, {
       headers: {
         Authorization:
           "Bearer eyJhbGciOiJIUzUxMiJ9.eyJyb2xlIjoiUk9MRV9VU0VSIiwidXNlckVtYWlsIjoiZG9pdGZvcmp1bmdAa2FrYW8uY29tIiwidXNlcklkIjozLCJpc3MiOiJpc3N1ZXIiLCJpYXQiOjE2NzU3Mjg3MzIsImV4cCI6MTY3NTczMjMzMn0.VwUVfFJBvWXuXRYQx8_dOqJr_iaNcohu6EiiMiWK_yqyHBLUP2eQZgG1YWyvgvLqurAsP6iWf5nSzbPnUr75zg",
