df4314c38cd44dc1d54b3cd4973b25480fed883d
정혜주 <fixup719@gmail.com>
2023-02-07 17:23:38 +0900

    Feat : calendar regist api

--- frontend/src/Router.tsx
+++ frontend/src/Router.tsx
@@ -9,34 +9,32 @@ import MyStudyPages from "pages/MyStudyPages";
 import KakaoPages from "pages/KakaoPages";
 function Router() {
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route exact path="/">
-          <LandingPages />
-        </Route>
-        <Route path="/manage">
-          <StudyManagePages />
-        </Route>
-        <Route path="/myStudy">
-          <MyStudyPages />
-        </Route>
-        <Route path="/test">
-          <TestPages />
-        </Route>
-        <Route path="/create">
-          <StudyCreatePages />
-        </Route>
-        <Route path="/test2">
-          <VideoRoomComponent />
-        </Route>
-        <Route path="/test3">
-          <WaitingPages />
-        </Route>
-        <Route path="/kakao/redirect/:accessToken">
-          <KakaoPages />
-        </Route>
-      </Switch>
-    </BrowserRouter>
+    <Switch>
+      <Route exact path="/">
+        <LandingPages />
+      </Route>
+      <Route path="/manage">
+        <StudyManagePages />
+      </Route>
+      <Route path="/myStudy">
+        <MyStudyPages />
+      </Route>
+      <Route path="/test">
+        <TestPages />
+      </Route>
+      <Route path="/create">
+        <StudyCreatePages />
+      </Route>
+      <Route path="/test2">
+        <VideoRoomComponent />
+      </Route>
+      <Route path="/test3">
+        <WaitingPages />
+      </Route>
+      <Route path="/kakao/redirect/:accessToken">
+        <KakaoPages />
+      </Route>
+    </Switch>
   );
 }
 
