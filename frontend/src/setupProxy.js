26f575a1b2d7d50c69ccdef2329a24e8cfccf597
정혜주 <fixup719@gmail.com>
2023-02-07 16:40:08 +0900

    Test : 실시간 채팅 test

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -1,18 +1,11 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = (app) => {
-  // app.use(
-  //   "/ws",
-  //   createProxyMiddleware({
-  //     target: "https://i8b205.p.ssafy.io/manage",
-  //     ws: true,
-  //   }),
-  // );
-  // app.use(
-  //   "/api",
-  //   createProxyMiddleware({
-  //     target: "https://i8b205.p.ssafy.io/be-api/studies",
-  //     ws: true,
-  //   }),
-  // );
+  app.use(
+    "/ws",
+    createProxyMiddleware({
+      target: "https://i8b205.p.ssafy.io/manage",
+      ws: true,
+    }),
+  );
 };
