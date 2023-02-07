4b8b27ce07ca03d7a064ede429b31fccb2f44610
정혜주 <fixup719@gmail.com>
2023-02-07 14:35:44 +0900

    Test : kakaoTest

--- frontend/src/pages/KakaoPages.tsx
+++ frontend/src/pages/KakaoPages.tsx
@@ -1,5 +1,5 @@
 import { LoginToken } from "atoms/LoginAtom";
-import { useEffect } from "preact/hooks";
+import { useEffect } from "react";
 import { useHistory, useParams } from "react-router-dom";
 import { useRecoilState } from "recoil";
 
@@ -7,17 +7,13 @@ function KakaoPages() {
   const params = useParams();
   const history = useHistory();
 
-  console.log("pramas 타입", typeof params);
   console.log("params : ", params);
 
+  const [token, setToken] = useRecoilState(LoginToken);
   useEffect(() => {
-    console.log("useEffect 실행");
-  });
-  // const [token, setToken] = useRecoilState(LoginToken);
-  // useEffect(() => {
-  //   console.log("param 타입 ", typeof params);
-  //   // history.push("/");
-  // }, [params]);
+    setToken(params);
+    history.push("/");
+  }, [params]);
 
   return <div></div>;
 }
