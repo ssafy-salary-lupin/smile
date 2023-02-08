5300fc1942df343d047bd28486d011c1d3a13ce6
jongseong <eello.bdev@gmail.com>
2023-02-08 12:49:27 +0900

    Fix : 시큐리티 설정 수정 / jwt 예외처리 추가

--- backend/smile/src/main/java/cp/smile/auth/jwt/JwtAuthenticationEntryPoint.java
+++ backend/smile/src/main/java/cp/smile/auth/jwt/JwtAuthenticationEntryPoint.java
@@ -1,5 +1,10 @@
 package cp.smile.auth.jwt;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import cp.smile.config.response.CommonResponse;
+import cp.smile.config.response.ResponseService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
@@ -9,11 +14,26 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+import static cp.smile.auth.jwt.JwtAuthenticationFilter.*;
+
 @Component
 public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
 
+    private static ObjectMapper objectMapper = new ObjectMapper();
+
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
-        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "인증이 필요합니다.");
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.setContentType("application/json");
+        String exception = (String) request.getAttribute(TOKEN_EXCEPTION_KEY);
+
+        CommonResponse commonResponse = new CommonResponse();
+        commonResponse.setCode(HttpStatus.UNAUTHORIZED.value());
+        commonResponse.setIsSuccess(false);
+        commonResponse.setMessage(exception);
+
+        if (!response.isCommitted()) {
+            response.getWriter().write(objectMapper.writeValueAsString(commonResponse));
+        }
     }
 }
