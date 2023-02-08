9d64a35e8761edeffd0015070c4c42379f7cb806
oth54477 <oth5447@naver.com>
2023-02-09 01:04:49 +0900

    Feat : API 테스트

--- frontend/src/apis/StudySearchApi.ts
+++ frontend/src/apis/StudySearchApi.ts
@@ -4,7 +4,9 @@ const BASE_URL = `https://i8b205.p.ssafy.io/be-api/studies`;
 
 export async function StudySearcAll() {
   try {
-    return await axios.get(`${BASE_URL}`);
+    const res = await axios.get(`${BASE_URL}`);
+    console.log(res);
+    return res;
   } catch (error: any) {
     // 에러 발생 시
     console.log(error);
