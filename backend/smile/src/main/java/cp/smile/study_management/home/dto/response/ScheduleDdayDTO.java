c9795a1a0ec817c4ca5def79ba403ae1f5ed3713
lsh80165 <lsh80165@gmail.com>
2023-02-10 10:24:29 +0900

    Feat : 스터디 홈 화면 구성

---
+++ backend/smile/src/main/java/cp/smile/study_management/home/dto/response/ScheduleDdayDTO.java
@@ -0,0 +1,7 @@
+package cp.smile.study_management.home.dto.response;
+
+import lombok.Getter;
+
+@Getter
+public class ScheduleDdayDTO {
+}
---
+++ backend/smile/src/main/java/cp/smile/study_management/home/service/homeServiceImpl.java
@@ -0,0 +1,14 @@
+package cp.smile.study_management.home.service;
+
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@RequiredArgsConstructor
+@Service
+public class homeServiceImpl {
+
+
+}
