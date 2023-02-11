9361264d99b03b31bceb4784f96577b552c9a2ad
lsh80165 <lsh80165@gmail.com>
2023-02-11 14:25:25 +0900

    Fix : 타임존 변경 - UTC -> KST

--- backend/smile/src/main/java/cp/smile/SmileApplication.java
+++ backend/smile/src/main/java/cp/smile/SmileApplication.java
@@ -5,11 +5,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import java.sql.Time;
+import java.util.TimeZone;
+
 @EnableJpaAuditing
 @SpringBootApplication
 public class SmileApplication {
 
 	public static void main(String[] args) {
+
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
 		SpringApplication.run(SmileApplication.class, args);
 	}
 
