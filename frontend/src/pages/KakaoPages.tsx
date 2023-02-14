79ad73a99098e1a49adfd765cb760ea51be3d823
oth54477 <oth5447@naver.com>
2023-02-14 13:36:58 +0900

    Feat : 사용자 정보 상태 관리

--- frontend/src/pages/KakaoPages.tsx
+++ frontend/src/pages/KakaoPages.tsx
@@ -1,5 +1,5 @@
 import { LoginState } from "atoms/LoginAtom";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useHistory, useParams } from "react-router-dom";
 import { useRecoilState } from "recoil";
 import { UserIdState } from "atoms/UserInfoAtom";
@@ -14,16 +14,19 @@ function KakaoPages() {
 
   const [tokenState, setTokenState] = useRecoilState(LoginState);
   const [userIdState, setUserIdState] = useRecoilState(UserIdState);
+  const [decoded, setDecoded] = useState();
   const onJoin = async () => {
     if (params.accessToken !== null) {
-      var decoded: any = jwt_decode(params.accessToken);
+      const decodeData = jwt_decode(params.accessToken);
+      console.log(decodeData);
     } else {
       console.log("none");
     }
 
-    await console.log(decoded?.userId);
-    await setUserIdState(decoded?.userId);
-    await window.location.replace(`/myStudy/${userIdState}`); // 새로고침해야 token null 값 해결 돼서 임시방편으로 바꿈 ㅠ interceptor하는 법 찾아보기
+    // console.log(decoded?.userId);
+    // setUserIdState(decoded?.userId);
+    // window.location.replace(`/myStudy/${userIdState}`);
+    // 새로고침해야 token null 값 해결 돼서 임시방편으로 바꿈 ㅠ interceptor하는 법 찾아보기
   };
 
   useEffect(() => {
