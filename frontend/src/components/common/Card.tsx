bb416e5a18488db880cbba2b94d12691c40e5282
oth54477 <oth5447@naver.com>
2023-02-14 10:03:18 +0900

    Feat : 방문 시간 표시

--- frontend/src/components/common/Card.tsx
+++ frontend/src/components/common/Card.tsx
@@ -279,6 +279,36 @@ export default function Card(props: PropsType) {
   const profileImgUrl = props.studyInfo.leader.imagePath;
   const studyImgUrl = props.studyInfo.imagePath;
 
+  const formatter = new Intl.RelativeTimeFormat("ko", { numeric: "auto" });
+
+  const passedTime = (date: string) => {
+    const start: any = new Date(date);
+    const end: any = new Date(); // 현재 날짜
+
+    const diff = (end - start) / 1000; // 경과 시간
+
+    const times = [
+      { name: "년", milliSeconds: 60 * 60 * 24 * 365 },
+      { name: "개월", milliSeconds: 60 * 60 * 24 * 30 },
+      { name: "일", milliSeconds: 60 * 60 * 24 },
+      { name: "시간", milliSeconds: 60 * 60 },
+      { name: "분", milliSeconds: 60 },
+    ];
+
+    // 년 단위부터 알맞는 단위 찾기
+    for (const value of times) {
+      const betweenTime = Math.floor(diff / value.milliSeconds);
+
+      // 큰 단위는 0보다 작은 소수 단위 나옴
+      if (betweenTime > 0) {
+        return `${betweenTime}${value.name} 전`;
+      }
+    }
+
+    // 모든 단위가 맞지 않을 시
+    return "방금 전";
+  };
+
   return (
     <Link
       style={{ textDecoration: "none", color: "black" }}
@@ -325,7 +355,13 @@ export default function Card(props: PropsType) {
             />
             <SCardUserItem>
               <span>{props.studyInfo.leader.nickname}</span>
-              <span>{visitedTime} min read</span>
+              <span>
+                {props.studyInfo.lastVisitTime
+                  ? passedTime(props.studyInfo.lastVisitTime)
+                  : props.studyInfo.lastVisitedTime
+                  ? passedTime(props.studyInfo.lastVisitedTime)
+                  : "방금 전"}
+              </span>
             </SCardUserItem>
           </SCardUser>
         </SCardItem>
