c00094c786576389c9213569952c4668578a4227
lsh80165 <lsh80165@gmail.com>
2023-02-14 22:57:21 +0900

    Fix : 스터디 업데이트시, 파일 업로드 가능하도록 수정

--- backend/smile/src/main/java/cp/smile/entity/study_common/StudyInformation.java
+++ backend/smile/src/main/java/cp/smile/entity/study_common/StudyInformation.java
@@ -9,6 +9,7 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.DynamicUpdate;
 
 import javax.persistence.*;
 import java.time.LocalDate;
@@ -22,6 +23,7 @@ import java.util.Set;
 @Getter
 @NoArgsConstructor
 @Entity
+@DynamicUpdate
 @Table(name = "study_informations")
 public class StudyInformation extends BaseEntity {
 
