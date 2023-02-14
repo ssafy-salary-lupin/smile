b7c744fbc4a8703224ba78e2d86b71786b426451
oth54477 <oth5447@naver.com>
2023-02-14 13:28:32 +0900

    Feat : 사용자 정보 상태 관리

--- frontend/src/pages/KakaoPages.tsx
+++ frontend/src/pages/KakaoPages.tsx
@@ -41,7 +41,7 @@ function KakaoPages() {
 
     onJoin();
 
-    window.location.replace("/myStudy/userIdState"); // 새로고침해야 token null 값 해결 돼서 임시방편으로 바꿈 ㅠ interceptor하는 법 찾아보기
+    window.location.replace(`/myStudy/${userIdState}`); // 새로고침해야 token null 값 해결 돼서 임시방편으로 바꿈 ㅠ interceptor하는 법 찾아보기
     return () => {
       onJoin();
     };
