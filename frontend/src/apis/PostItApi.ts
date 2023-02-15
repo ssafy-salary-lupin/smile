c93b8aabb7a6d51aa70d1a7266fbf13f54e2aad4
oth54477 <oth5447@naver.com>
2023-02-16 00:07:49 +0900

    Fix : my study

--- frontend/src/apis/PostItApi.ts
+++ frontend/src/apis/PostItApi.ts
@@ -1,11 +1,11 @@
 import axios from "axios";
 
-// const BASE_URL = `https://i8b205.p.ssafy.io/be-api`;
-const BASE_URL = `/be-api`;
+const BASE_URL = `https://i8b205.p.ssafy.io/be-api`;
+// const BASE_URL = `/be-api`;
 
-// const token = localStorage.getItem("kakao-token");
-const token =
-  "eyJhbGciOiJIUzUxMiJ9.eyJyb2xlIjoiUk9MRV9VU0VSIiwidXNlckVtYWlsIjoib3RoNTQ0N0BuYXZlci5jb20iLCJ1c2VySWQiOjYsImlzcyI6Imlzc3VlciIsImlhdCI6MTY3NjQ2NTQxNCwiZXhwIjoxNjc2NTUxODE0fQ.bOovDIT89-5SwwVcdiE4jM4-0LWetRNrPYR9V2NYdUKMFHCZIE05tkxpMOgilZQ5BDwpVpq0b9Cm7LSZe7-Sgw";
+const token = localStorage.getItem("kakao-token");
+// const token =
+//   "eyJhbGciOiJIUzUxMiJ9.eyJyb2xlIjoiUk9MRV9VU0VSIiwidXNlckVtYWlsIjoib3RoNTQ0N0BuYXZlci5jb20iLCJ1c2VySWQiOjYsImlzcyI6Imlzc3VlciIsImlhdCI6MTY3NjQ2NTQxNCwiZXhwIjoxNjc2NTUxODE0fQ.bOovDIT89-5SwwVcdiE4jM4-0LWetRNrPYR9V2NYdUKMFHCZIE05tkxpMOgilZQ5BDwpVpq0b9Cm7LSZe7-Sgw";
 
 const api = axios.create({
   baseURL: `${BASE_URL}`,
