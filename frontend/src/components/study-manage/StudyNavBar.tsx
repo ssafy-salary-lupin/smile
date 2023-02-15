e6cc221b4a8b20a99e40a7203fe0781fd5c77da4
songkira <thdrlfk93@naver.com>
2023-02-15 16:17:36 +0900

    Feat : 스터디원 관리 페이지

--- frontend/src/components/study-manage/StudyNavBar.tsx
+++ frontend/src/components/study-manage/StudyNavBar.tsx
@@ -65,6 +65,7 @@ const Itemline = styled.ul`
   display: flex;
   align-items: center;
 `;
+const Box = styled.div``;
 
 interface Data {
   code: number;
@@ -109,12 +110,14 @@ function StudyNavBar() {
         <ItemText>
           <Link to={`/manage/meetingRecord/${studyId}`}>화상 회의</Link>
         </ItemText>
-        <Itemline>|</Itemline>
-        <ItemText>
-          {studyCeo === userId ? (
-            <Link to={`/manage/manageMember/${studyId}`}>스터디 관리</Link>
-          ) : null}
-        </ItemText>
+        {studyCeo === userId ? (
+          <Box>
+            <Itemline>|</Itemline>
+            <ItemText>
+              <Link to={`/manage/manageMember/${studyId}`}>스터디 관리</Link>
+            </ItemText>
+          </Box>
+        ) : null}
       </Items>
     </StudyNav>
   );
