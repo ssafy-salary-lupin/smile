227c3b03fe733ebec65fc0feb9e000b5a0b892e7
oth54477 <oth5447@naver.com>
2023-02-15 09:08:54 +0900

    Fix : 내 스터디 수정

--- frontend/src/pages/MyStudyPages.tsx
+++ frontend/src/pages/MyStudyPages.tsx
@@ -1,22 +1,15 @@
 import React, { useState } from "react";
 import { MyStudyApi } from "apis/MyStudyApi";
 import { useQuery } from "react-query";
-import { useRecoilValue } from "recoil";
 import { Link, useParams } from "react-router-dom";
 import { BackgroundYellow } from "components/common/BackgroundYellow";
 import styled from "styled-components";
 import Card from "components/common/Card";
 import BlankSpace from "components/common/BlankSpace";
 import SearchComponent from "components/common/SearchComponent";
-import { CaretDown, CaretUp } from "components/common/DuotonIcons";
 import MyStudyNotFound from "components/common/MyStudyNotFound";
 
 import PostIt from "components/common/PostIt";
-
-const SearchBar = styled.div`
-  display: flex;
-  justify-content: center;
-`;
 
 const Wrapper = styled.div``;
 
@@ -65,19 +58,6 @@ const Additionalection = styled.div`
 
 const GraphContainer = styled.div``;
 
-const PurposeBox = styled.div`
-  width: 50vw;
-  height: 27.778vw;
-  background-color: #fffbf0;
-  border-radius: 15px;
-  @media screen and (min-width: 1680px) {
-    width: 720px;
-    height: 400.003px;
-  }
-`;
-
-const PurposeContainer = styled.div``;
-
 const StudyContainer = styled.div`
   display: flex;
 `;
@@ -95,12 +75,15 @@ const Cards = styled.div<NumberOfCardsProps>`
   display: grid;
   grid-template-columns: repeat(3, 31.48vw);
   /* grid-template-rows: repeat(2, 38.889vw); */
-  grid-template-rows: repeat(${(props) => props.NumberOfCards}, 38.889vw);
+  grid-template-rows: repeat(${(props) => props.NumberOfCards / 3}, 38.889vw);
   margin-top: 2.8vw;
   @media screen and (min-width: 1680px) {
     grid-template-columns: repeat(3, 453.312px);
     /* grid-template-rows: repeat(2, 560.002px); */
-    grid-template-rows: repeat(${(props) => props.NumberOfCards}, 560.002px);
+    grid-template-rows: repeat(
+      ${(props) => props.NumberOfCards / 3},
+      560.002px
+    );
     margin-top: 40.32px;
   }
 `;
@@ -114,6 +97,7 @@ const CardWrapper = styled.div`
 const SearchContainer = styled.div`
   display: flex;
   justify-content: center;
+  flex-direction: column;
 `;
 
 interface StudiesInterface {
@@ -164,101 +148,6 @@ interface NumberOfCardsProps {
   NumberOfCards: number;
 }
 
-const DdayBox = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: space-around;
-  width: 94.444vw;
-  height: 27.778vw;
-  background-color: #fffbf0;
-  border-radius: 15px;
-  @media screen and (min-width: 1680px) {
-    width: 1680px;
-    height: 400.003px;
-  }
-`;
-
-// const DdayBox = styled.div`
-//   overflow-y: scroll;
-//   height: 17.778vw;
-//   &::-webkit-scrollbar {
-//     width: 6px;
-//   }
-//   &::-webkit-scrollbar-thumb {
-//     height: 17%;
-//     background-color: ${(props) => props.theme.pointColor};
-//     border-radius: 10px;
-//   }
-// `;
-
-const Dday = styled.div`
-  cursor: pointer;
-  width: 90%;
-  min-height: 2.778vw;
-  height: auto;
-  border: 1px solid rgb(118, 118, 118);
-  box-shadow: 2px 2px 2px rgb(169, 169, 169);
-  border-radius: 0.556vw;
-  display: flex;
-  flex-direction: row;
-  margin-bottom: 0.556vw;
-`;
-
-const Tag = styled.div`
-  background-color: #314e8d;
-  border-radius: 0.556vw 0 0 0.556vw;
-  width: 15%;
-`;
-
-const Text = styled.div`
-  width: 85%;
-  font-size: 0.972vw;
-  line-height: 1.667vw;
-  text-align: left;
-  display: flex;
-  align-items: center;
-  padding: 0 1.111vw;
-`;
-
-const DdayItems = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: space-around;
-`;
-
-const DdayItem = styled.div``;
-
-const DdayContent = styled.div`
-  display: flex;
-  justify-content: flex-start;
-  div {
-    :nth-child(1) {
-    }
-    :nth-child(2) {
-    }
-    span {
-      font-size: 32px;
-      font-weight: 600;
-    }
-  }
-`;
-
-const BlueBox = styled.div`
-  border-radius: 15px 0 0 15px;
-  background-color: #314e8d;
-  width: 48px;
-  height: 48px;
-`;
-
-const ContentBox = styled.div`
-  display: flex;
-
-  border-radius: 0 15px 15px 0;
-  width: 500px;
-  height: 48px;
-  background-color: white;
-`;
-
 export default function MyStudyPages() {
   // 유저 아이디
   const userId = useParams<Iparams>().userId;
@@ -279,71 +168,6 @@ export default function MyStudyPages() {
 
   const foldNOpenEnd = () => {
     setIsOpenEnd(!isOpenEnd);
-  };
-
-  const dDay = [
-    {
-      startTime: "2023-02-13T15:01:51.047Z",
-      title: "일정 1",
-    },
-    {
-      startTime: "2023-02-14T15:01:51.047Z",
-      title: "일정 2",
-    },
-    {
-      startTime: "2023-02-15T15:01:51.047Z",
-      title: "일정 3",
-    },
-  ];
-
-  interface DdayInfo {
-    isSuccess: boolean;
-    code: number;
-    message: string;
-    result: {
-      id: number;
-      day: number;
-      title: string;
-      studyName: string;
-    }[];
-  }
-
-  const ddayInfo: DdayInfo = {
-    isSuccess: true,
-    code: 200,
-    message: "성공",
-    result: [
-      {
-        id: 1,
-        day: 1,
-        title: "스터디 참여",
-        studyName: "SSAFY",
-      },
-      {
-        id: 2,
-        day: 2,
-        title: "OO기업 서류 마감일",
-        studyName: "대전 스터디",
-      },
-      {
-        id: 3,
-        day: 3,
-        title: "OO시험 접수 마감",
-        studyName: "OO시험 스터디",
-      },
-      {
-        id: 4,
-        day: 4,
-        title: "스터디 참여",
-        studyName: "OO 스터디",
-      },
-      {
-        id: 5,
-        day: 5,
-        title: "OO기업 면접",
-        studyName: "면접 스터디",
-      },
-    ],
   };
 
   return (
