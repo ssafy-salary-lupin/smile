8a1ce335c1bfdffa530ded9ace33b30bb381aa4d
lsh80165 <lsh80165@gmail.com>
2023-02-16 09:52:46 +0900

    Fix : 데이터가 저장되지 않는 현상 수정

--- backend/smile/src/main/java/cp/smile/study_management/chat/controller/ChatController.java
+++ backend/smile/src/main/java/cp/smile/study_management/chat/controller/ChatController.java
@@ -74,7 +74,6 @@ public class ChatController {
 
             chatService.enterChatRoom(chatMessageDTO.getRoomId());
 
-            // TODO : SenderId는 식별자이므로 이름을 조회해와서 던져주는 로직이 필요하다.
             chatMessageDTO.setMessage(chatMessageDTO.getSenderName() + "님이 입장하셨습니다.");
         }
 
